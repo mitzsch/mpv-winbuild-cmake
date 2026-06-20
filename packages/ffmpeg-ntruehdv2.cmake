ab2280f497fb0b61a48f61d06b721022f0d9bf0b
Alex Mitzsch <43502191+mitzsch@users.noreply.github.com>
2026-06-20 11:11:03 +0200

    Add ffmpeg-ntruehdv2/mpv-plex-ntruehdv2
    This adds the capability to build ffmpeg/mpv with a new TrueHD fix

---
+++ packages/ffmpeg-ntruehdv2.cmake
@@ -0,0 +1,126 @@
+ExternalProject_Add(ffmpeg-ntruehdv2
+    DEPENDS
+        amf-headers
+        avisynth-headers
+        ${nvcodec_headers}
+        bzip2
+        lame
+        lcms2
+        openssl
+        libssh
+        libsrt
+        libass
+        libbluray
+        libdvdnav
+        libdvdread
+        libmodplug
+        libpng
+        libsoxr
+        libbs2b
+        libvpx
+        libwebp
+        libzimg
+        libmysofa
+        fontconfig
+        harfbuzz
+        opus
+        speex
+        vorbis
+        x264
+        ${ffmpeg_x265}
+        xvidcore
+        libxml2
+        libvpl
+        libopenmpt
+        libjxl
+        shaderc
+        libplacebo
+        libzvbi
+        libaribcaption
+        aom
+        svtav1
+        dav1d
+        vapoursynth
+        ${ffmpeg_uavs3d}
+        ${ffmpeg_davs2}
+        rubberband
+        libva
+        openal-soft
+    GIT_REPOSITORY https://github.com/mitzsch/FFmpeg.git
+    SOURCE_DIR ${SOURCE_LOCATION}
+    GIT_CLONE_FLAGS "--sparse --filter=tree:0"
+    GIT_CLONE_POST_COMMAND "sparse-checkout set --no-cone /* !tests/ref/fate"
+    UPDATE_COMMAND ""
+    GIT_REMOTE_NAME origin
+    GIT_TAG master-4
+    CONFIGURE_COMMAND ${EXEC} <SOURCE_DIR>/configure
+        --cross-prefix=${TARGET_ARCH}-
+        --prefix=${MINGW_INSTALL_PREFIX}
+        --arch=${TARGET_CPU}
+        --target-os=mingw32
+        --pkg-config-flags=--static
+        --enable-cross-compile
+        --enable-runtime-cpudetect
+        --enable-gpl
+        --enable-version3
+        --enable-avisynth
+        --enable-vapoursynth
+        --enable-libass
+        --enable-libbluray
+        --enable-libdvdnav
+        --enable-libdvdread
+        --enable-libfreetype
+        --enable-libfribidi
+        --enable-libfontconfig
+        --enable-libharfbuzz
+        --enable-libmodplug
+        --enable-libopenmpt
+        --enable-libmp3lame
+        --enable-lcms2
+        --enable-libopus
+        --enable-libsoxr
+        --enable-libspeex
+        --enable-libvorbis
+        --enable-libbs2b
+        --enable-librubberband
+        --enable-libvpx
+        --enable-libwebp
+        --enable-libx264
+        --enable-libx265
+        --enable-libaom
+        --enable-libsvtav1
+        --enable-libdav1d
+        ${ffmpeg_davs2_cmd}
+        ${ffmpeg_uavs3d_cmd}
+        --enable-libxvid
+        --enable-libzimg
+        --enable-openssl
+        --enable-libxml2
+        --enable-libmysofa
+        --enable-libssh
+        --enable-libsrt
+        --enable-libvpl
+        --enable-libjxl
+        --enable-libplacebo
+        --enable-libshaderc
+        --enable-libzvbi
+        --enable-libaribcaption
+        ${ffmpeg_cuda}
+        --enable-amf
+        --enable-openal
+        --enable-opengl
+        --disable-doc
+        --enable-vaapi
+        --disable-vdpau
+        --disable-videotoolbox
+        --disable-decoder=libaom_av1
+        ${ffmpeg_lto}
+        --extra-cflags='-Wno-error=int-conversion'
+        "--extra-libs='${ffmpeg_extra_libs}'" # -lstdc++ / -lc++ needs by libjxl and shaderc
+    BUILD_COMMAND ${MAKE}
+    INSTALL_COMMAND ${MAKE} install
+    LOG_DOWNLOAD 1 LOG_UPDATE 1 LOG_CONFIGURE 1 LOG_BUILD 1 LOG_INSTALL 1
+)
+
+force_rebuild_git(ffmpeg-ntruehdv2)
+cleanup(ffmpeg-ntruehdv2 install)
