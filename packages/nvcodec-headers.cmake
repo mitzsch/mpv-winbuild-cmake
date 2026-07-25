547c50c5d14cebf6fd76e92678a6d835c48a673b
Alex Mitzsch <43502191+mitzsch@users.noreply.github.com>
2026-07-25 10:12:14 +0200

    Remove --filter=tree:0 from nvcodec-headers

--- packages/nvcodec-headers.cmake
+++ packages/nvcodec-headers.cmake
@@ -1,7 +1,6 @@
 ExternalProject_Add(nvcodec-headers
     GIT_REPOSITORY https://git.videolan.org/git/ffmpeg/nv-codec-headers.git
     SOURCE_DIR ${SOURCE_LOCATION}
-    GIT_CLONE_FLAGS "--filter=tree:0"
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ""
     BUILD_COMMAND ""
